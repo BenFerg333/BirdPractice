188bafb8266ff0f78404ecb6c4cf060eb4d16460
24BFerg <106261176+24BFerg@users.noreply.github.com>
2024-03-25 11:10:21 -0700

    Worked on dropdown
    Worked on dropdown

--- app.js
+++ app.js
@@ -28,6 +28,19 @@ $("#reveal").on("click", function(){
 	$(".audio").append("<p>" + birdNames[count - 1][0] + "</p>")
 });
 
+$('#form1').submit(function() {
+    // get all the inputs into an array.
+    var $inputs = $('#form1 :input');
+
+    // not sure if you wanted this, but I thought I'd add it.
+    // get an associative array of just the values.
+    var values = {};
+    $inputs.each(function() {
+        values[this.name] = $(this).val();
+    });
+    console.log(values);
+});
+
 var birdForm = $("#birds");
 birdForm.onchange = formSubmit;
 
